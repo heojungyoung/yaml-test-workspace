bdb20b9d8d7baf90d901ee9a07bd614a91e75a4f
heojungyoung <tyireal@gmail.com>
2022-12-30 12:37:23 +0900

    mmmmmmm

---
+++ src/main/java/com/example/check/Info.java
@@ -0,0 +1,12 @@
+package com.example.check;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Info {
+    private List<String> checkversion = new ArrayList<>();
+}

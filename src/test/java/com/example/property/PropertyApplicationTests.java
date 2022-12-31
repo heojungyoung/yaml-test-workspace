19f5eb189ecdea9f91e4ce8d7be3489e1690fc70
heojungyoung <tyireal@gmail.com>
2022-12-31 11:23:53 +0900

    gradle test

--- src/test/java/com/example/property/PropertyApplicationTests.java
+++ src/test/java/com/example/property/PropertyApplicationTests.java
@@ -16,9 +16,6 @@ class PropertyApplicationTests {
     @Value("${val.text}")
     private String val;
 
-    // @Autowired
-    // private Prop prop;
-
     @Value("${serial.number}")
     private String list;
 
@@ -29,17 +26,10 @@ class PropertyApplicationTests {
     private String ver;
 
     @Test
-    void propTest(){
+    public void propTest(){
 
         System.out.println("val : " + val);
-      /*  System.out.println("prop arr : " + prop.getArr().toString());
-        System.out.println("prop arr2 : " + prop.getArr2().toString());
-        System.out.println("prop arr3[0].a : " + prop.getArr3().get(0).getA());
-        System.out.println("prop arr3[0].b : " + prop.getArr3().get(0).getB());
-        System.out.println("prop arr3[0].c : " + prop.getArr3().get(0).getC());
-        System.out.println("prop arr3[1].a : " + prop.getArr3().get(1).getA());
-        System.out.println("prop arr3[1].b : " + prop.getArr3().get(1).getB());
-        System.out.println("prop arr3[1].c : " + prop.getArr3().get(1).getC());*/
+
 
         System.out.println("list : " + list);
 
